3b4e83433e6c5e040de1f1d70c586ce20b943d67
15bstrobel <15bstrobel@gmail.com>
2015-11-09 11:55:49 -0800

    Added Brick, updated scene
    Edit ball prefab to 200 speed
    Added brick + brick script
    Updated Scene to show this

---
+++ 2Player Pong/Assets/Brick.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brick : MonoBehaviour {
+
+	public int strength;
+	int currentStrength;
+
+	// Use this for initialization
+	void Start () {
+		currentStrength = strength;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D hit) {
+		if (hit.gameObject.CompareTag("Ball")) {
+			currentStrength--;
+			if (currentStrength <= 0) {
+				Destroy(gameObject);
+			}
+		}
+	}
+
+}
