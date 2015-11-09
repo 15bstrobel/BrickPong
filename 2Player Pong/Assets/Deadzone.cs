7dc3b1988c3a3d0e4394d67b63b80359cd9fe723
Akashosaur <akasharora15@gmail.com>
2015-11-09 12:11:33 -0800

    DeadZone
    deadzone&&brick

---
+++ 2Player Pong/Assets/Deadzone.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Deadzone : MonoBehaviour {
+	
+	void Start () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Destroy (other.gameObject);
+	}
+
+	void Update () {
+	
+	}
+}
