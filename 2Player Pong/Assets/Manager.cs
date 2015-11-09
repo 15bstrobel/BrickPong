1b67614cce338c149d0d971cd174f408707e3a75
15bstrobel <15bstrobel@gmail.com>
2015-11-09 11:38:41 -0800

    Version left off from original effort

---
+++ 2Player Pong/Assets/Manager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Manager : MonoBehaviour {
+
+	public GameObject player1;
+	public GameObject player2;
+
+	public float min;
+	public float max;
+	public KeyCode pauseKey; 
+
+	bool pause = false;
+
+	// Use this for initialization
+	void Start () {
+		player1.GetComponent<PlayerMove>().setMinMax(min, max);
+		player2.GetComponent<PlayerMove>().setMinMax(min, max);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(pauseKey)) {
+			pause = !pause;
+			if (pause) {
+				Time.timeScale = 0f;
+			} else {
+				Time.timeScale = 1f;
+			}
+		}
+
+	}
+}
